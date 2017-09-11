0f57d48543fb7119ac04a6b14c44dfdbc088744b
cabana <usman.na.khan@gmail.com>
2017-09-11 07:11:06 +0000

    working demo code w rpi

--- pb-webapp.py
+++ pb-webapp.py
@@ -4,24 +4,33 @@ import time
 
 app = Flask(__name__)
 
+gpio.setmode(gpio.BCM)
+gpio.setup(18, gpio.OUT)
+pwm = gpio.PWM(18, 100)
+
+def update(angle):
+  duty = float(angle) / 10.0 + 2.5
+  pwm.ChangeDutyCycle(duty)
+
 @app.route('/feedPet')
 def feedPet():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(23, gpio.OUT)
-    gpio.output(23, 1)
-    print("feeding pet")
-    time.sleep(1)
-    gpio.output(23, 0)
-    print("done feeding pet")
-    return render_template('index.html')
+  update(93)
+  time.sleep(0.5)
+  update(0)
+
+  #we can set pwm to 0 to stop servo from making noise
+  #at the closed position
+  #time.sleep(1)
+  #pwm.ChangeDutyCycle(0)
+  print("done feeding pet")
+  return render_template('index.html')
+
 
 @app.route("/")
 def main():
-    gpio.setmode(gpio.BCM)
-    gpio.setup(23, gpio.OUT)
-    time.sleep(1)
-    gpio.output(23,0)
-    return render_template('index.html')
+  pwm.start(5)
+  time.sleep(1)
+  return render_template('index.html')
 if __name__ == "__main__":
     # app.run(host='0.0.0.0', port=8000, debug=True)
     app.run(host='0.0.0.0', port=8070, debug=True)
